4dceb05952dc0d08d3bca378225d9fdecd00c7d0
JustinWhitehouse <justin.t.whitehouse@gmail.com>
2023-01-15 17:33:00 +0000

    section 3 and exercises.

---
+++ ex/3.3.6/1/app.R
@@ -0,0 +1,25 @@
+library(shiny)
+
+ui <- fluidPage(
+  textInput("name", "What is your name?"),
+  textOutput("greeting")
+)
+
+server1 <- function(input, output, server) {
+  output$greeting <- renderText(paste0("Hello ", input$name))
+}
+
+server2 <- function(input, output, server) {
+  greeting <- reactive(paste0("Hello ", input$name))
+  output$greeting <- renderText(greeting())
+}
+
+server3 <- function(input, output, server) {
+  output$greeting <- renderText(paste0("Hello ", input$name))
+}
+
+#server <- server1
+#server <- server2
+server <- server3
+
+shinyApp(ui,server)
